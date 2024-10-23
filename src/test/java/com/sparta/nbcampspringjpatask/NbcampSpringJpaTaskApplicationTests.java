2eacdde2bfa989eee5ffc87b3db7f5ba046e4973
ballqs <46590521+ballqs@users.noreply.github.com>
2024-10-23 20:46:11 +0900

    spring boot test 주석처리

--- src/test/java/com/sparta/nbcampspringjpatask/NbcampSpringJpaTaskApplicationTests.java
+++ src/test/java/com/sparta/nbcampspringjpatask/NbcampSpringJpaTaskApplicationTests.java
@@ -3,11 +3,11 @@ package com.sparta.nbcampspringjpatask;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class NbcampSpringJpaTaskApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class NbcampSpringJpaTaskApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
